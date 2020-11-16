fec7d0ec2107a79ea66d3159c7b6cb25c3ed3a07
Yaowen <760735372@qq.com>
2020-11-16 23:59:21 +0800

    C Primer Plus Six One

---
+++ CPrimerPlusTwo/first.c
@@ -0,0 +1,15 @@
+//
+// Created by 张耀文 on 2020/11/16.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    num = 1;
+    printf("I am a simple");
+    printf("My favorite number is %d because it is first.\t", num);
+    getchar();
+    return 0;
+}
